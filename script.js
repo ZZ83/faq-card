044bf6354d2755c59baa9c34bc7f6f5cebe722f5
Zekel Keys <zzkeys83@gmail.com>
2021-05-11 12:56:02 -0700

    Remove button and svg element and add image element

--- script.js
+++ script.js
@@ -7,17 +7,17 @@ questions.forEach( question => {
                 answer.classList.remove("show-answer");
                 answer.classList.add("hide-answer");
                 answer.parentNode.firstElementChild.firstElementChild.style.fontWeight = "400";
-                answer.parentNode.firstElementChild.lastElementChild.firstElementChild.style.transform = "rotate(0deg)";
+                answer.parentNode.firstElementChild.lastElementChild.style.transform = "rotate(0deg)";
             });
             question.lastElementChild.classList.remove("hide-answer");
             question.lastElementChild.classList.add("show-answer");
             question.firstElementChild.firstElementChild.style.fontWeight = "bold";
-            question.firstElementChild.lastElementChild.firstElementChild.style.transform = "rotate(180deg)";
+            question.firstElementChild.lastElementChild.style.transform = "rotate(180deg)";
         } else if (question .lastElementChild.classList.contains("show-answer")) {
             question.lastElementChild.classList.remove("show-answer");
             question.lastElementChild.classList.add("hide-answer");
             question.firstElementChild.firstElementChild.style.fontWeight = "400";
-            question.firstElementChild.lastElementChild.firstElementChild.style.transform = "rotate(0deg)";
+            question.firstElementChild.lastElementChild.style.transform = "rotate(0deg)";
         }
     });
 });
